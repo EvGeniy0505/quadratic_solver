3cbfc5f8a84625fc617b9283cfb9459a73a74f11
Evgeniy Rogov <rogoff050505@gmail.com>
2024-09-07 02:52:06 +0300

    with some errors. More functions

--- main.cpp
+++ main.cpp
@@ -1,164 +1,261 @@
-
-
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
 
+// TODO это все варианты количества корней?
 enum quantity_roots
 {
     ZERO_ROOTS,
     ONE_ROOT,
-    TWO_ROOTS
+    TWO_ROOTS,
+    EQUAl = -1
 };
 
 
 struct roots   
 {
+    int quantity_of_roots;
     double x_1;
     double x_2;        
 };
 
+struct coeffs
+{
+    double first;
+    double second;
+    double third;
+};
+
 void buff_clean();
-
-roots struct_konstr(double first_root, double second_root);
-
-int how_many_roots(double first_coef, double second_coef, double third_coef);
 
 roots count_roots(double first_coef, double second_coef, double third_coef);
 
+int equal_null(double var);
+
+coeffs input_coeffs();
+
+void output_answ(roots final_roots);
+
+bool check_input_yes();
+
+bool check_input_no();
+
+
+
 int main()
 {
-    double a = 0, b = 0, c = 0;
+    
 
-    int flag = 0;
+    char user_answer[4] = {};
 
-    char* user_answer = (char*) calloc(3, sizeof(char));
+    roots answ_of_quadr_solver = {};
 
-    roots final_roots = struct_konstr(0, 0);
+    coeffs final_coeffs = {};
+
+    const char* yes_strings[] = {
+        "yes",
+        "да",
+        "хочу",
+        "сэр да сэр"
+    }; 
+
+    const char* no_strings[] = {
+        "no",
+        "нет",
+        "иди нахуй",
+        "неа"
+    }; 
+
 
     printf("Hi, man! Введи 3 числа, коэффициентов квадратного уравнения, чтобы у тебя не порвалось очко\n");
 
     do
     {
-        if(strcmp(user_answer, "да") == 0 || strcmp(user_answer, "Да") == 0)
-        {
-            printf("Okay, man. Вводи коэффициенты:\n");
-        }
+        //dialog_with_user_2();
+        
+        final_coeffs = input_coeffs();
 
-        flag = scanf("%lf %lf %lf", &a, &b, &c);
+        answ_of_quadr_solver = count_roots(final_coeffs.first, final_coeffs.second, final_coeffs.third);
 
-        while(flag != 3)
-        {
-            buff_clean();
-
-            printf("Чё за хуйня??? Быстро введи ещё раз нормально коффициенты в одну строку через пробел\n");
-
-            flag = scanf("%lf %lf %lf", &a, &b, &c);
-            
-        }
-        switch(how_many_roots(a, b, c))
-        {
-            case TWO_ROOTS:
-                final_roots = count_roots(a, b, c);
-                printf("x_1 = %lf, x_2 = %lf\n", final_roots.x_1, final_roots.x_2);
-                printf("Не, ну 2 корня у уравнения это заебись\n");
-                break;
-            case ONE_ROOT:
-                final_roots = count_roots(a, b, c);
-                printf("x_1 = %lf\n", final_roots.x_1);
-                printf("Ура!!! Один корень!!! Ты наконец-то узнал длину своего члена!\n");  
-                break;
-            case ZERO_ROOTS:
-                printf("Корней у уравнения нет нихуя, пошел в пизду, милый друг\n");
-                break;
-            default:
-                printf("ERROR!!! Sorry, твоё очко cry\n");
-        }
+        output_answ(answ_of_quadr_solver);
     
         printf("Бляха, еще решать эту хуйню хочешь?\n");
-        scanf("%s", user_answer);
+
+
+
+        //dialog_with_user_1();
         
-        while((strcmp(user_answer, "нет") != 0) && (strcmp(user_answer, "Нет") != 0) && (strcmp(user_answer, "да") != 0) && (strcmp(user_answer, "Да") != 0))
-        {
-            printf("Да заебал, введи нормально ответ, да да, нет нет\n");
-            scanf("%s", user_answer);
-        }
         
     } while((strcmp(user_answer, "да") == 0) || (strcmp(user_answer, "Да") == 0));
 
     return 0;
 }
 
+bool check_input_yes_or_no(const char* all_strings)
+{
+
+    char user_answer[256] = {};
+
+    scanf("%s", user_answer);
+
+    for(int i = 0; i < sizeof(all_strings)/sizeof(all_strings[-10000]); i++)
+    {
+        if(strcasecmp(all_strings[i], user_answer) == 0)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+char* dialog_with_user_2(char user_answer[4])
+{
+    if(strcmp(user_answer, "да") == 0 || strcmp(user_answer, "Да") == 0)
+    {
+        printf("Okay, man. Вводи коэффициенты:\n");
+    }
+
+    return user_answer;
+}
+
+
+char dialog_with_user_1(char user_answer[4])
+{
+    char user_answer[4] = {};
+
+    scanf("%s", user_answer);
+        
+    while((strcmp(user_answer, "нет") != 0) && (strcmp(user_answer, "Нет") != 0) && (strcmp(user_answer, "да") != 0) && (strcmp(user_answer, "Да") != 0))
+    {
+        printf("Да заебал, введи нормально ответ, да да, нет нет\n");
+        scanf("%s", user_answer);
+    }
+
+    return user_answer;
+} 
+
 
 roots count_roots(double first_coef, double second_coef, double third_coef)
 {
     double eps = 0.0000001;
 
-    double x_1 = 0, x_2 = 0;
+    double x_1 = 0, x_2 = 0, det = 0;
 
-    x_1 = (- second_coef + sqrt(second_coef * second_coef - 4 * first_coef * third_coef)) / (2 * first_coef);
+    det = (second_coef * second_coef - 4 * first_coef * third_coef);
+    
+    roots all_roots;
 
-    x_2 = (- second_coef - sqrt(second_coef * second_coef - 4 * first_coef * third_coef)) / (2 * first_coef);
-
-    roots all_roots = struct_konstr(x_1, x_2);
-
-    if(abs(x_1 - x_2) < eps)
+    if(equal_null(first_coef) && equal_null(second_coef) && equal_null(third_coef))
     {
-        all_roots.x_2 = 0;
-        return all_roots;
+        all_roots.quantity_of_roots = EQUAl;
     }
-    else
+
+    else if(equal_null(first_coef))
     {
-        return all_roots;
+        all_roots.x_1 = (- third_coef) / (second_coef);
+
+        all_roots.quantity_of_roots = ONE_ROOT;
     }
-}
 
-int how_many_roots(double first_coef, double second_coef, double third_coef)
-{
-    double det = 0;
-    double eps = 0.0000001;
-
-    det = second_coef * second_coef - 4 * first_coef * third_coef;
-
-    if(det > eps)
+    else if(det > eps)
     {    
-        return TWO_ROOTS;
+        det = sqrt(det);
+
+        all_roots.x_1 = (- second_coef + (det)) / (2 * first_coef);
+
+        all_roots.x_2 = (- second_coef - (det)) / (2 * first_coef);
+
+        all_roots.quantity_of_roots = TWO_ROOTS;
     }
     else if(det < -eps)
     {
-        return ZERO_ROOTS;
+        all_roots.quantity_of_roots = ZERO_ROOTS;
     }
-    else if(det < eps && det > -eps)
+    else if(equal_null(det))
     {
-        return ONE_ROOT;
+        all_roots.x_1 = (- second_coef) / (2 * first_coef);
+
+        all_roots.quantity_of_roots = ONE_ROOT;
     }
-    else
-    {
-        return -1;
-    }
+
+    return all_roots;
 }
 
 void buff_clean()
 {
     int next_symb = 0;
     next_symb = getchar();
-    while(next_symb != '\n')
+    while(next_symb != '\n' || next_symb != EOF)
     {
         next_symb = getchar();
     }
 }
 
-
-
-roots struct_konstr(double first_root, double second_root)
+int equal_null(double var)
 {
-    roots quadratic;
+    double eps = 0.0000001;
 
-    quadratic.x_1 = first_root;
-
-    quadratic.x_2 = second_root;
-
-    return quadratic;
+    if(var < eps && var > -eps)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
+coeffs input_coeffs()
+{
+    double a = 0, b = 0, c = 0;
+
+    int right_numb_coeffs = 0;
+
+    right_numb_coeffs = scanf("%lf %lf %lf", &a, &b, &c);
+
+    coeffs all_coeffs;
+
+    while(right_numb_coeffs != 3)
+    {
+        buff_clean();
+
+        printf("Чё за хуйня??? Быстро введи ещё раз нормально коффициенты в одну строку через пробел\n");
+
+        right_numb_coeffs = scanf("%lf %lf %lf", &a, &b, &c);
+        
+    }
+
+    all_coeffs.first  = a;
+    all_coeffs.second = b;
+    all_coeffs.third  = c;
+
+    return all_coeffs;
+}
+
+void output_answ(roots final_roots)
+{
+    switch(final_roots.quantity_of_roots)
+    {
+        case TWO_ROOTS:
+            printf("x_1 = %lf, x_2 = %lf\n", final_roots.x_1, final_roots.x_2);
+            printf("Не, ну 2 корня у уравнения это заебись\n");
+            break;
+        case ONE_ROOT:
+            printf("x_1 = %lf\n", final_roots.x_1);
+            printf("Ура!!! Один корень!!! Ты наконец-то узнал длину своего члена!\n");  
+            break;
+        case ZERO_ROOTS:
+            printf("Корней у уравнения нет нихуя, пошел в пизду, милый друг\n");
+            break;
+        case EQUAl:
+            printf("Блять, ну получил ты что 0 = 0 и чего ты добивался???\n");
+            break;
+        default:
+            printf("ERROR!!! Sorry, твоё очко cry\n");
+    }
+}
+
+// ВО-ПЕРВЫХ Я ТЕБЯ ОЧЕНЬ ЛЮБЛЮ, А ВО-ВТОРЫХ Я ХОЧУ С ТОБОЙ ПОБОТАТЬ ВМЕСТЕ!!!!! К Полине Новиковой
